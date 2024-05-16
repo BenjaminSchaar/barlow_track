b7280f141cd318bae6af9cbc91a6ab89c603b4f9
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-05-16 12:08:30 +0200

    Add setup.py

---
+++ barlow_track/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='barlow_track',
+      version='0.1.0',
+      description='Track using modified Barlow Twins',
+      url='https://github.com/Charles-Fieseler-Vienna/barlow_track',
+      author='Charles Fieseler',
+      author_email='charles.fieseler@gmail.com',
+      license='MIT',
+      packages=setuptools.find_namespace_packages(),
+      zip_safe=False)
