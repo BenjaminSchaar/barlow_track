2928e823da4a300274b9f6a5ed357a3d36fadddf
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-05-17 17:10:18 +0200

    Update slurm options

--- barlow_track/scripts/optimize_hyperparameters.py
+++ barlow_track/scripts/optimize_hyperparameters.py
@@ -62,7 +62,8 @@ if DEBUG:
 else:
     cluster = 'slurm'
 executor = AutoExecutor(folder="/tmp/submitit_runs", cluster=cluster)
-executor.update_parameters(slurm_time="0-6:00:00")
+executor.update_parameters(timeout_min=60 * 12)
+executor.update_parameters(slurm_time="0-06:00:00")
 executor.update_parameters(cpus_per_task=4)
 executor.update_parameters(slurm_partition="basic,gpu")
 executor.update_parameters(slurm_job_name="barlow_hyperparameter_search")
