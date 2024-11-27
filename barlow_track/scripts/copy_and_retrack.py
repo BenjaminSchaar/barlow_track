18976970e43eb5d628568994444272c53b71de73
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-11-27 11:32:27 +0100

    Do not crash if the project was already copied

--- barlow_track/scripts/copy_and_retrack.py
+++ barlow_track/scripts/copy_and_retrack.py
@@ -2,24 +2,27 @@ from wbfm.pipeline.traces import full_step_4_make_traces_from_config
 from wbfm.utils.projects.project_config_classes import make_project_like
 from wbfm.utils.projects.finished_project_data import ProjectData
 from barlow_track.utils.track_using_barlow import track_using_barlow_from_config
+from pathlib import Path
 
 
 def copy_and_retrack(original_project, target_directory, model_fname):
     """
     Copy a project and retrack it
     """
-    # fname = "/lisc/scratch/neurobiology/zimmer/zihaozhai/WBFM/project/2024-10-02_13-46_SWF1088_2per_worm4-2024-10-02/project_config.yaml"
-    # fname = "/lisc/scratch/neurobiology/zimmer/zihaozhai/WBFM/project/2024-10-02_14-56_SWF1088_2per_worm8-2024-10-02/project_config.yaml"
     steps_to_keep = ['preprocessing', 'segmentation', 'training_data']
-    # target_directory = '/lisc/scratch/neurobiology/zimmer/fieseler/wbfm_projects_future/barlow_challenging_datasets/neuropal'
 
     # Copy
-    target_project_name = make_project_like(original_project, target_directory=target_directory,
-                                            steps_to_keep=steps_to_keep)
+    try:
+        target_project_name = make_project_like(original_project, target_directory=target_directory,
+                                                steps_to_keep=steps_to_keep)
+    except FileExistsError:
+        print(f"Project already exists in {target_directory}. Skipping copy step and continuing.")
+        # Generate name in the same way as make_project_like
+        project_dir = Path(original_project).parent
+        target_project_name = Path(target_directory).joinpath(project_dir.name)
 
     # Load and retrack
     project_data = ProjectData.load_final_project_data_from_config(target_project_name)
-    # model_fname = '/lisc/scratch/neurobiology/zimmer/wbfm/TrainedBarlow/hyperparameter_search_neuropal/resnet50.pth'
     track_using_barlow_from_config(project_data.project_config, model_fname=model_fname)
 
     # Make and save full traces
