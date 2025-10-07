63026ec92e4496c2b059fb685f119912acc054d7
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-10-07 16:09:41 +0200

    Change paper script

--- barlow_track/scripts/paper_scripts/paper_make_projects_and_run_pipeline.py
+++ barlow_track/scripts/paper_scripts/paper_make_projects_and_run_pipeline.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     model_parent_dir = "/lisc/data/scratch/neurobiology/zimmer/wbfm/TrainedBarlow/"
     projects_parent_dir = "/lisc/data/scratch/neurobiology/zimmer/fieseler/barlow_track_paper/analyzed_projects/"
 
-    trained_model_dirs = ['untrained']
+    trained_model_dirs = ['untrained_zimmer', 'untrained_flavell', 'untrained_samuel', 'untrained_leifer',]
     base_lab_names = ['zimmer', 'flavell', 'samuel', 'leifer']
     
     DEBUG = False
@@ -25,6 +25,6 @@ if __name__ == "__main__":
             new_location = [os.path.join(projects_parent_dir, lab_name, '_'.join(parts[:-1])) for lab_name in base_lab_names]
         models_dir = os.path.join(model_parent_dir, model_name)
         
-        create_projects_and_traces_from_barlow_folder(new_location, models_dir)
+        create_projects_and_traces_from_barlow_folder(new_location, models_dir, use_label_propagation=True)
 
     print("Finished; please check the SLURM queue for running jobs.")
