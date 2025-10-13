e681878a4c30d25c12575d3b51ce94c746a41d11
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-10-13 14:20:28 +0200

    Update order of categories

--- barlow_track/utils/utils_paper.py
+++ barlow_track/utils/utils_paper.py
@@ -47,9 +47,18 @@ def calculate_nearest_neighbor_distance(df, return_all=False):
 
 BASE_COLORMAP = plotly.colors.qualitative.D3
 def paper_colormap():
-    _cmap = {'Zimmer': BASE_COLORMAP[0], 'Leifer': BASE_COLORMAP[2],# 'Samuel': BASE_COLORMAP[2], 
-             'Flavell': BASE_COLORMAP[1]}
+    _cmap = {'Zimmer': BASE_COLORMAP[0], 'Leifer': BASE_COLORMAP[1],# 'Samuel': BASE_COLORMAP[2], 
+             'Flavell': BASE_COLORMAP[2]}
     cmap = _cmap.copy()
     for k, v in _cmap.items():
         cmap[k.lower()] = v
     return cmap
+
+
+def paper_category_order():
+    _cmap = {'Dataset': ['Zimmer', 'Leifer', 'Flavell']}
+    cmap = _cmap.copy()
+    for k, v in _cmap.items():
+        cmap[k.lower()] = v
+    return cmap
+
