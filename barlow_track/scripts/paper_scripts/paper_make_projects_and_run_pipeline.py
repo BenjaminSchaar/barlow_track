82f57b86923a4e37311e4f4981590260c216ad6d
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-10-20 08:38:49 +0200

    Paper script

--- barlow_track/scripts/paper_scripts/paper_make_projects_and_run_pipeline.py
+++ barlow_track/scripts/paper_scripts/paper_make_projects_and_run_pipeline.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
                           #  'training_data_sweep_flavell',
                           #  'training_data_sweep_zimmer',
                           #  'hyperparameter_sweep_leifer'
-                          'inverse_augmentation_sweep_flavell',
-                          'inverse_augmentation_sweep_leifer',
-                          'inverse_augmentation_sweep_zimmer',
+                          'augmentation_sweep_flavell', 'inverse_augmentation_sweep_only_original_loss_flavell',
+                          'augmentation_sweep_leifer', 'inverse_augmentation_sweep_only_original_loss_leifer',
+                          'augmentation_sweep_zimmer', 'inverse_augmentation_sweep_only_original_loss_zimmer'
                           ]
     
     rule_opts = {'zimmer': {'target_rule': "traces"}, 'flavell': {'target_rule': "traces"}, 
